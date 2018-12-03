ab1951bc643f1920b3704f8ca4eef377252a11a4
ody1205 <36085613+ody1205@users.noreply.github.com>
2018-12-03 12:16:27 -0600

    Final_project_first_work

---
+++ Final_Project.m
@@ -0,0 +1,34 @@
+clear
+close all
+load('kernel_GT_1.mat')
+image = im2double(imread('image_1.jpg'));
+image = imresize(image, 0.2);
+figure;
+imshow(image)
+title('Original Image')
+
+
+
+%%Weiner filter
+LEN = 21;
+THETA = 11;
+PSF_W = fspecial('motion', LEN, THETA);
+
+
+%%Gaussian filter
+hsize = 10;
+sigma = 5;
+PSF_G = fspecial('gaussian',hsize,sigma);
+
+T = convmtx2(PSF_W, [size(image)]);
+Y1 = reshape(T*image(:), size(PSF_W)+[size(image)]-1);
+figure, imshow(Y1)
+title('Blurred Image')
+Y1 = Y1(3:size(Y1,1)-2,11:size(Y1,2)-10);
+A = inv(T'*T)*T';
+X = reshape(A' * Y1(:), size(PSF_W)+[size(image)]-1);
+figure,imshow(X)
+title('Restored Image')
+
+
+
