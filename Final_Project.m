04885e31851089977ecf8eb2d7c574fc3113bce8
Jialuo Gao <jialuogao980513@gmail.com>
2018-12-08 18:18:53 -0600

    almost done

--- Final_Project.m
+++ Final_Project.m
@@ -1,40 +1,95 @@
+%% setting up
 clear
 close all
-image = im2double(imread('image_1.jpg'));
-image = imresize(image, 0.2);
+image = im2double(rgb2gray(imread('bucky.jpg')));
+image = imresize(image, 0.3);
 figure;
 imshow(image)
 title('Original Image')
 
-
-
-%%Weiner filter
-LEN = 21;
-THETA = 11;
-PSF_W = fspecial('motion', LEN, THETA);
-
-
-%%Gaussian filter
-hsize = 10;
+%% generate different filters
+% Gaussian filter
+hsize = 5;
 sigma = 5;
-PSF_G = fspecial('gaussian',hsize,sigma);
+Gas = fspecial('gaussian',hsize,sigma);
+% Motion filter (Weiner)
+LEN = 5;
+THETA = 5;
+Mtn = fspecial('motion', LEN, THETA);
+% Averaging filter
+hsize = 5;
+Avg = fspecial('average',hsize);
+% Edge filter
+Edg = [0 -1 0; -1 4 -1; 0 -1 0];
+% Sharpen filter
+Shr = [0 -1 0; -1 5 -1; 0 -1 0];
 
-filter = PSF_W
-T = convmtx2(filter, [size(image)]);
-Y1 = reshape(T*image(:), size(filter)+[size(image)]-1);
-figure, imshow(Y1)
+%% blurring using Gaussian filter 
+filter = Gas;
+blurred_img = conv2(image, filter, 'same');
+figure, imshow(blurred_img);
 title('Blurred Image')
 
-Y1_minRow = ceil(size(filter,1)/2);
-Y1_maxRow = size(Y1,1)-floor(size(filter,1)/2);
-Y1_minCol = ceil(size(filter,2)/2);
-Y1_maxCol = size(Y1,2)-floor(size(filter,2)/2);
-Y1 = Y1(Y1_minRow:Y1_maxRow, Y1_minCol:Y1_maxCol);
+%% deblurring by solving least square
+T = convmtx2(filter, size(image));
+A = (T'*T)\T';
+deblurred_img = reshape(A' * blurred_img(:), size(filter)+size(image)-1);
+% resize the blurred image
+b_minRow = ceil(size(filter,1)/2);
+b_maxRow = size(deblurred_img,1)-floor(size(filter,1)/2);
+b_minCol = ceil(size(filter,2)/2);
+b_maxCol = size(deblurred_img,2)-floor(size(filter,2)/2);
+deblurred_img = deblurred_img(b_minRow:b_maxRow, b_minCol:b_maxCol);
 
-A = inv(T'*T)*T';
-X = reshape(A' * Y1(:), size(filter)+[size(image)]-1);
-figure,imshow(X)
+figure,imshow(deblurred_img)
 title('Restored Image')
 
+%% add noise to the blurred image
+noise = rand(size(image))*0.01;
+blurred_img = blurred_img - noise;
+figure;imshow(blurred_img);
+title('Blurred Image With Noise')
 
+%% deblurring by solving least square
+A = (T'*T)\T';
+deblurred_img = reshape(A' * blurred_img(:), size(filter)+size(image)-1);
+% resize the blurred image
+b_minRow = ceil(size(filter,1)/2);
+b_maxRow = size(deblurred_img,1)-floor(size(filter,1)/2);
+b_minCol = ceil(size(filter,2)/2);
+b_maxCol = size(deblurred_img,2)-floor(size(filter,2)/2);
+deblurred_img = deblurred_img(b_minRow:b_maxRow, b_minCol:b_maxCol);
 
+figure,imshow(deblurred_img)
+title('Restored Noisy Image with LS')
+
+%% deblurring by solving least square
+r = 25;
+[U,S,V] = svd(blurred_img);
+U = U(:,1:r);
+S = S(1:r,1:r);
+V = V(:,1:r);
+blurred_img_svd = U*S*V';
+
+deblurred_img = reshape(A' * blurred_img_svd(:), size(filter)+size(image)-1);
+% resize the blurred image
+b_minRow = ceil(size(filter,1)/2);
+b_maxRow = size(deblurred_img,1)-floor(size(filter,1)/2);
+b_minCol = ceil(size(filter,2)/2);
+b_maxCol = size(deblurred_img,2)-floor(size(filter,2)/2);
+deblurred_img = deblurred_img(b_minRow:b_maxRow, b_minCol:b_maxCol);
+figure,imshow(deblurred_img)
+title('Restored Noisy Image with SVD Low Rank Approximation and LS')
+
+%% deblurring by solving least square
+A = (T'*T + 0.005*eye(size(T,2),size(T,2)))\T';
+deblurred_img = reshape(A' * blurred_img(:), size(filter)+size(image)-1);
+% resize the blurred image
+b_minRow = ceil(size(filter,1)/2);
+b_maxRow = size(deblurred_img,1)-floor(size(filter,1)/2);
+b_minCol = ceil(size(filter,2)/2);
+b_maxCol = size(deblurred_img,2)-floor(size(filter,2)/2);
+deblurred_img = deblurred_img(b_minRow:b_maxRow, b_minCol:b_maxCol);
+
+figure,imshow(deblurred_img)
+title('Restored Noisy Image with LS with Tikhonov')
